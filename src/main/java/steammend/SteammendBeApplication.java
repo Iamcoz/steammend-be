268fcbfa36298ca6ded5f95f2ce2e7ec76d94cbc
coz <cozisdev@gmail.com>
2022-10-19 15:24:25 +0900

    create spring boor project

---
+++ src/main/java/steammend/SteammendBeApplication.java
@@ -0,0 +1,13 @@
+package steammend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteammendBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteammendBeApplication.class, args);
+	}
+
+}
