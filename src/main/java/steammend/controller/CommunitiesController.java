7a3f59bf5b06ff165aacc439dc1297a5add217f3
coz <cozisdev@gmail.com>
2022-11-07 15:40:25 +0900

    import문, pom.xml정리

--- src/main/java/steammend/controller/CommunitiesController.java
+++ src/main/java/steammend/controller/CommunitiesController.java
@@ -9,7 +9,6 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import steammend.model.dto.CommunitiesDTO;
